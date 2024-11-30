a5960e9ef9d9b4c63feeb3028fe05836100b255a
SumanRasaili <rasailisuman123@gmail.com>
2024-11-30 14:29:41 +0545

    added design size

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      designSize: const Size(375, 812),
       splitScreenMode: true,
       child: MaterialApp(
         title: 'Oriflame Nepal',
